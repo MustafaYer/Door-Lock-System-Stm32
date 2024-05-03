16fd30c2f34135bb2938da5154f6e8b564c4d686
Mustafa Yer <mustafayer45@gmail.com>
2024-05-03 14:53:03 +0300

    Update i2c-lcd.c

--- Core/Src/i2c-lcd.c
+++ Core/Src/i2c-lcd.c
@@ -54,97 +54,6 @@ void lcd_put_cur(int row, int col) {
 void lcd_init(void) {
 	// 4 bit initialisation
 
-//	if(next == 0){
-//		timeout = milis + 50;
-//		next = 1;
-//	}else if(next == 1){
-//		if(milis > timeout){
-//			next = 2;
-//		}
-//	}else if(next == 2){
-//		lcd_send_cmd(0x30);
-//		next = 3;
-//	}else if(next == 3){
-//		timeout = milis + 5;
-//		next = 4;
-//	}else if(next == 4){
-//		if(milis > timeout){
-//			next = 5;
-//		}
-//	}else if(next == 5){
-//		lcd_send_cmd(0x30);
-//		next = 6;
-//	}else if(next == 6){
-//		timeout = milis + 1;
-//		next = 7;
-//	}else if(next == 7){
-//		if(milis > timeout){
-//			next = 8;
-//		}
-//	}else if(next == 8){
-//		lcd_send_cmd(0x30);
-//		next = 9;
-//	}else if(next == 9){
-//		timeout = milis + 10;
-//		next = 10;
-//	}else if(next == 10){
-//		if(milis > timeout){
-//			next = 11;
-//		}
-//	}else if(next == 11){
-//		lcd_send_cmd(0x20);
-//		next = 13;
-//	}else if(next == 13){
-//		timeout = milis + 10;
-//		next = 14;
-//	}else if(next == 14){
-//		if(milis > timeout){
-//			next = 15;
-//		}
-//	}else if(next == 15){
-//		lcd_send_cmd(0x28);
-//		next = 16;
-//	}else if(next == 16){
-//		timeout = milis + 1;
-//		next = 17;
-//	}else if(next == 17){
-//		if(milis > timeout){
-//			next = 18;
-//		}
-//	}else if(next == 18){
-//		lcd_send_cmd(0x08);
-//		next = 19;
-//	}else if(next == 19){
-//		timeout = milis + 1;
-//		next = 20;
-//	}else if(next == 20){
-//		if(milis > timeout){
-//			next = 21;
-//		}
-//	}else if(next == 21){
-//		lcd_send_cmd(0x01);
-//		next = 22;
-//	}else if(next == 22){
-//		timeout = milis + 2;
-//		next = 23;
-//	}else if(next == 23){
-//		if(milis > timeout){
-//			next = 24;
-//		}
-//	}else if(next == 24){
-//		lcd_send_cmd(0x06);
-//		next = 25;
-//	}else if(next == 25){
-//		timeout = milis + 1;
-//		next = 26;
-//	}else if(next == 26){
-//		if(milis > timeout){
-//			next = 27;
-//		}
-//	}else if(next == 27){
-//		lcd_send_cmd(0x0C);
-//		next = 1000;
-//	}
 	HAL_Delay(60);  // wait for >40ms
 	lcd_send_cmd(0x30);
 	HAL_Delay(5);  // wait for >4.1ms
@@ -176,10 +85,3 @@ void lcd_send_string(char *str) {
 }
 
 
-
-//void lcd_send_int(int number) {
-//	char buffer[50];
-//	sprintf(buffer, "%d", number);
-//
-//	lcd_send_string(buffer);
-//}
